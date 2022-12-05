63d0f5da3b18141921046d7eab96ac0f0f1cbb74
umang111 <51570750+umang111@users.noreply.github.com>
2022-12-05 18:22:20 +0530

    Delete userService directory

--- userService/src/main/java/com/userService/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.userService.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.userService.app.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
