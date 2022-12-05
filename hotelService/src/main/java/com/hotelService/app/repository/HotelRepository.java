18ea1e977479529da1cdd2ac6846daba5090b06d
umang111 <51570750+umang111@users.noreply.github.com>
2022-12-05 18:22:07 +0530

    Delete hotelService directory

--- hotelService/src/main/java/com/hotelService/app/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotelService.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotelService.app.entity.Hotel;
-
-public interface HotelRepository  extends JpaRepository<Hotel, Integer>{
-
-}
